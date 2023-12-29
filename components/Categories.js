ec124be4a058fad87b2b28f4bb9dbd2c41c5b555
nblack0917 <nickablack@gmail.com>
2023-12-29 14:58:21 -0600

    1:27 into RN crash course. First Commit

---
+++ components/Categories.js
@@ -0,0 +1,46 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+    >
+      {/* Category Card */}
+
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 3"
+      />
+      {/* <Text>Categories</Text> */}
+    </ScrollView>
+  );
+};
+
+export default Categories;
